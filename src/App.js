999b10e9071ba651e2ada138ec95296bbfb0f060
Karam Daddy <ka-rump.daddy@hotmail.com>
2017-06-13 13:55:54 +1200

    Removed default text in the header

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
           <ChordController />
       </div>
