e7e8791ce27b968582f11d2a93199b8baf8e8dfe
Karam Daddy <karam.daddy91@gmail.com>
2017-06-13 21:44:02 +1200

    Moved noSelection to generic css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import ChordController from './components/chordController';
 import './App.css';
 
@@ -7,8 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-        </div>
+        <div className="App-header noSelection" />
           <ChordController />
       </div>
     );
